cf388d9ab53ad82fc401aa6042e20a0890b2daac
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-06-15 15:34:49 +0900

    Update Data_visualization.py

--- Data_visualization.py
+++ Data_visualization.py
@@ -10,6 +10,9 @@ def plot(df):
     ilist = df["산업중분류별(2)"].unique().tolist()
     industries = st.multiselect("업종을 선택해주세요!", ilist[1:])
     st.subheader("{}을 선택해주셨네요.".format(", ".join(industries)))
+    dfs = {country: df[df["산업중분류별(2)"] == country] for country in countries}
+
+    st.wirte(dfs.items())
 
 if add_selectbox == "업종별":
     df = pd.read_csv("by_industry(17~21).csv", encoding='cp949')
