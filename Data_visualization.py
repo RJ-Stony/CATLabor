9dcaa44df2742bac40945ef107f279443c007f67
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-06-15 15:49:33 +0900

    Update Data_visualization.py

--- Data_visualization.py
+++ Data_visualization.py
@@ -11,7 +11,7 @@ def plot(df):
     industries = st.multiselect("업종을 선택해주세요!", ilist[1:])
     st.subheader("{}을 선택해주셨네요.".format(", ".join(industries)))
     index_number = df.index[df["산업중분류별(2)"] == industries]
-    st.write(df.iloc[index_number].to_list())
+    st.write(index_number)
     
     '''chart_data = pd.DataFrame(
         np.random.randn(20, 3),
