e5c186ec9fed6c38e31ac769105bb2d7c96018c0
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-06-15 14:57:50 +0900

    Update Data_visualization.py

--- Data_visualization.py
+++ Data_visualization.py
@@ -2,3 +2,7 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv("by_age_group(17~21).csv", encoding='cp949')
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(df)
